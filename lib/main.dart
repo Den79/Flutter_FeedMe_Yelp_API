8ddf00ec06497f51442b4cdfbbb941f36fac296a
Denis Maltev <A01171996@ad.bcit.ca>
2020-03-10 10:55:15 -0700

    Getting location - works with Nexus5Q
    - Tapping into Device Geolocator
    - Future Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutterapp/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:io';
 import 'dart:convert';
@@ -10,9 +11,11 @@ class MyApp extends StatelessWidget {
 
   void fetchBusinessList() async {
     await DotEnv().load('.env');
+    Location location = new Location();
+    await location.getLocation();
     var response = await http.get(
       'https://api.yelp.com/v3/businesses/search' +
-          "?&latitude=49.2820&longitude=-123.1171",
+          "?&latitude=${location.latitude}&longitude=${location.longitude}",
       headers: {
         HttpHeaders.authorizationHeader: "Bearer ${DotEnv().env['API_KEY']}"
       },

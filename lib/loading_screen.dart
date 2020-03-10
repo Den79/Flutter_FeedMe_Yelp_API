ddcdd1b9c18bae44bf2cfa2827637dd092a411d2
Denis Maltev <A01171996@ad.bcit.ca>
2020-03-10 14:03:48 -0700

    Code refactoring
    Refactor fetchBusinessList into business.dart service

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutterapp/services/business.dart';
-import 'package:flutterapp/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:io';
-import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,28 +13,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     // hit API and assign value ONCE when widget is added to the tree
-    _futureData = _fetchBusinessList();
+    _futureData = fetchBusinessList();
   }
-
-  // returns a Future asynchronously
-  Future<List<Business>> _fetchBusinessList() async {
-    await DotEnv().load('.env');
-    Location location = new Location();
-    await location.getLocation();
-    var response = await http.get(
-      'https://api.yelp.com/v3/businesses/search' +
-          "?&latitude=${location.latitude}&longitude=${location.longitude}",
-      headers: {
-        HttpHeaders.authorizationHeader: "Bearer ${DotEnv().env['API_KEY']}"
-      },
-    );
-    // get the businesses from the response
-    Iterable decodedData = jsonDecode(response.body)['businesses'];
-    // extract the names to a list
-    List<Business> business = decodedData.map((businessJson) => Business.fromJson(businessJson)).toList();
-    return business;
-  }
-
   // render the future to the screen via FutureBuilder
   @override
   Widget build (BuildContext context){
@@ -74,7 +49,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
           return Text(snapshot.error);
         }
         // default show a loading spinner
-        return CircularProgressIndicator();
+        return Center (child: CircularProgressIndicator());
       });
   }
 }
--- lib/services/business.dart
+++ lib/services/business.dart
@@ -1,3 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutterapp/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'dart:convert';
+
 class Business {
   final String id;
   final String name;
@@ -12,4 +18,23 @@ class Business {
       image_url: json['image_url']
     );
   }
+}
+
+// returns a Future asynchronously
+Future<List<Business>> fetchBusinessList() async {
+  await DotEnv().load('.env');
+  Location location = new Location();
+  await location.getLocation();
+  var response = await http.get(
+    'https://api.yelp.com/v3/businesses/search' +
+        "?&latitude=${location.latitude}&longitude=${location.longitude}",
+    headers: {
+      HttpHeaders.authorizationHeader: "Bearer ${DotEnv().env['API_KEY']}"
+    },
+  );
+  // get the businesses from the response
+  Iterable decodedData = jsonDecode(response.body)['businesses'];
+  // extract the names to a list
+  List<Business> business = decodedData.map((businessJson) => Business.fromJson(businessJson)).toList();
+  return business;
 }
